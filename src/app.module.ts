8157dc0966ccb3f74e7fe0306b25292aea4dfe33
Jakub Woźny <jakub_wozny_01@wp.pl>
2021-02-07 16:56:38 +0100

    add missing TypeOrmModule import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReservationModule } from './reservation/reservation.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ReservationModule, UserModule],
+  imports: [TypeOrmModule.forRoot(), ReservationModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })

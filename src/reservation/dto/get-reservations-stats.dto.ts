03f172642425401d8e60bf06b957742ecbef589b
Jakub Woźny <jakub_wozny_01@wp.pl>
2021-02-08 22:18:01 +0100

    make interval dates of GetReservationsStatsDto optional

--- src/reservation/dto/get-reservations-stats.dto.ts
+++ src/reservation/dto/get-reservations-stats.dto.ts
@@ -1,11 +1,13 @@
-import { Matches } from 'class-validator';
+import { IsOptional, Matches } from 'class-validator';
 
 const datePattern = /^\d{4}-\d{2}-\d{2}$/;
 
 export class GetReservationsStatsDto {
+  @IsOptional()
   @Matches(datePattern)
   intervalStartDate: string;
 
+  @IsOptional()
   @Matches(datePattern)
   intervalEndDate: string;
 }
